2225d92234c41a00d592bb512e25ff02922f0e93
umayer1 <umayerhossain518@gmail.com>
2024-01-30 23:13:44 +0600

    Frind

---
+++ frinds.js
@@ -0,0 +1,112 @@
+var allofimg =document.querySelector("#all-img");
+
+
+function num1(){
+ allofimg.src = "umayer.jpg";
+ document.querySelector("#para").innerHTML = "Umayer Hossain"
+}
+
+function num2(){
+    allofimg.src = "rixan.jpg";
+    document.querySelector("#para").innerHTML = "Rixan";
+   }
+
+function num3(){
+    allofimg.src = "akib.jpg";
+    document.querySelector("#para").innerHTML = "Akib";
+}
+// 
+
+function num4(){
+    allofimg.src = "rifat.jpg";
+    document.querySelector("#para").innerHTML = "Rifat";
+}
+function num5(){
+    allofimg.src = "emtiaz.jpg";
+    document.querySelector("#para").innerHTML = "Emtiaz";
+}
+function num6(){
+    allofimg.src = "tamims.jpg";
+    document.querySelector("#para").innerHTML = "Tamim";
+}
+function num7(){
+    allofimg.src = "jilani.jpg";
+    document.querySelector("#para").innerHTML = "Jilani";
+}
+function num8(){
+    allofimg.src = "naim.jpg";
+    document.querySelector("#para").innerHTML = "Naim";
+}
+
+function num9(){
+    allofimg.src = "nazim.jpg";
+    document.querySelector("#para").innerHTML = "Nazim";
+}
+
+function num10(){
+    allofimg.src = "al amin1.jpg";
+    document.querySelector("#para").innerHTML = "Al-Amin";
+}
+function num11(){
+    allofimg.src = "ridoy.jpg";
+    document.querySelector("#para").innerHTML = "Ridoy";
+}
+function num12(){
+    allofimg.src = "rabby.jpg";
+    document.querySelector("#para").innerHTML = "Rabby";
+}
+function num13(){
+    allofimg.src = "rakib.jpg";
+    document.querySelector("#para").innerHTML = "Rakib";
+}
+function num14(){
+    allofimg.src = "al amin2.jpg";
+    document.querySelector("#para").innerHTML = "Al-Amin";
+}
+
+function num15(){
+    allofimg.src = "rostom.jpg";
+    document.querySelector("#para").innerHTML = "Rostom";
+}
+function num16(){
+    allofimg.src = "bablu.jpg";
+    document.querySelector("#para").innerHTML = "Bablu";
+}
+function num17(){
+    allofimg.src = "sahadat.jpg";
+    document.querySelector("#para").innerHTML = "Sahadat";
+}
+function num18(){
+    allofimg.src = "sakib2.jpg";
+    document.querySelector("#para").innerHTML = "Sakib";
+}
+function num19(){
+    allofimg.src = "sakib.jpg";
+    document.querySelector("#para").innerHTML = "Sakib";
+}
+
+function num20(){
+    allofimg.src = "shovo.jpg";
+    document.querySelector("#para").innerHTML = "Shovo";
+}
+function num21(){
+    allofimg.src = "tamim.jpg";
+    document.querySelector("#para").innerHTML = "Tamim";
+}
+function num22(){
+    allofimg.src = "shovo2.jpg";
+    document.querySelector("#para").innerHTML = "Shovo";
+}
+
+function num23(){
+    allofimg.src = "Zihad.jpg";
+    document.querySelector("#para").innerHTML = "Zihad";
+}
+
+function num24(){
+    allofimg.src = "pritom.jpg";
+    document.querySelector("#para").innerHTML = "Pritom";
+}
+
+
+
---
+++ info.js
@@ -0,0 +1,27 @@
+var infoimg =document.querySelector("#i-img");
+
+function num1(){
+    infoimg.src = "umayer.jpg";
+    document.querySelector("#h1").innerHTML="Umayer Hossain";
+    document.querySelector(".p1").innerHTML="Web-developar(J.)";
+    document.querySelector("#all-span").innerHTML="MD : Umayer Hossain";
+    document.querySelector("#all-span1").innerHTML="Rofiqul Islam";
+    document.querySelector("#all-span2").innerHTML="Mansora akther";
+    document.querySelector("#all-span3").innerHTML="17-01-2006";
+    document.querySelector("#all-span4").innerHTML="+880184055582";
+}
+
+function num2(){
+    infoimg.src = "rixan.jpg";
+    document.querySelector("#h1").innerHTML="Rixan";
+    document.querySelector(".p1").innerHTML="Student";
+    document.querySelector("#all-span").innerHTML="Hridoy Sorkar";
+    document.querySelector("#all-span1").innerHTML="";
+    document.querySelector("#all-span2").innerHTML="";
+    document.querySelector("#all-span3").innerHTML="";
+    document.querySelector("#all-span4").innerHTML="";
+}
+
+function num3(){
+    
+}
